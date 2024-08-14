0daa024324ae9434b724ce468aa1ab9c67ef0927
JullianeSSousa <jullianecoda@gmail.com>
2024-08-13 21:48:35 -0300

    Finalizando decodificador

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function criptografar(){
-    alert('test')
+function criptografar() {
+    let valor = document.getElementById("caixa_de_texto").value
+    if(valor === ""){
+        alert('Nenhuma mensagem encontrada')
+        
+    }
+    let result = valor.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+    document.getElementById("resultado").value = result
+    document.getElementById("caixa_de_texto").value = ''
+}
+
+
+
+function decriptografar() {
+    let valor = document.getElementById("caixa_de_texto").value
+    if(valor === ""){
+        alert('Nenhuma mensagem encontrada')
+    }
+    let result = valor.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    document.getElementById("resultado").value = result
+    document.getElementById("caixa_de_texto").value = ''
+
+}
+
+function copiar() {
+    var copyText = document.getElementById("resultado");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    navigator.clipboard.writeText(copyText.value);
+    document.getElementById("resultado").value = ''
+    document.getElementById("caixa_de_texto").value = ''
 }
